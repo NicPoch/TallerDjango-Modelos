0ffc8f0d7439bbd56059eed3f95d8094adac1a95
Nicolas Poch Llorente <n.poch@uniandes.edu.co>
2020-08-21 10:23:18 -0500

    final funciona

--- alarms/models.py
+++ alarms/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 from measurements.models import Measurement
+import enum
 # Create your models here.
 class Alarm(models.Model):
     name=models.CharField(max_length=50)
     descripcion=models.CharField(max_length=200)
     dateTime=models.DateField(auto_now_add=True)
-    measurements=models.ManyToManyField(Measurement)
-
+    measurements=models.ManyToManyField(Measurement,related_name='alarms')
+    
     def __str__(self):
-        return 'Alarm %s (%s)'%(self.name,self.descripcion)
+        places=[]
+        for s in self.measurements.all():
+            places.append(s.place)
+        return 'Alarm %s (%s) of: %s'%(self.name,self.descripcion,', '.join(places))
--- measurements/models.py
+++ measurements/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 from variables.models import Variable
 
-
 # Create your models here.
 class Measurement(models.Model):
     variable=models.ForeignKey(Variable, on_delete=models.CASCADE)
     value=models.FloatField(null=True,blank=True,default=None)
     unit=models.CharField(max_length=50)
     place=models.CharField(max_length=50)
-    dateTime=models.DateField(auto_now_add=True)
+    dateTime=models.DateField(auto_now_add=True)    
 
     def __str__(self):
-        return '%s %s'%(self.value,self.unit)
+        names=[]
+        for s in self.alarms.all():
+            names.append(s.name)
+        return '%s %s %s %s, Alarms(%s)'%(self.variable,self.place,self.value,self.unit,', '.join(names))
